39ad9bca3a1cd228dae99af3aa9dd481474b3085
Daniel Schulz <faulolio@gmail.com>
2023-04-22 14:20:24 +0200

    Fix SpeedTracker

--- Assets/Scripts/Runtime/Relocator.cs
+++ Assets/Scripts/Runtime/Relocator.cs
@@ -8,6 +8,21 @@ namespace ZSBB {
         Rigidbody attachedRigidbody;
         [SerializeField]
         CapsuleCollider attachedCollider;
+        [SerializeField]
+        SpeedTracker baseTracker;
+        [SerializeField]
+        SpeedTracker topTracker;
+
+        public Vector3 GetVelocityAt(Vector3 position) {
+            float t = InverseLerp(baseTracker.position, topTracker.position, position);
+            return Vector3.Lerp(baseTracker.velocity, topTracker.velocity, t);
+        }
+
+        static float InverseLerp(Vector3 a, Vector3 b, Vector3 value) {
+            var ab = b - a;
+            var av = value - a;
+            return Vector3.Dot(av, ab) / ab.sqrMagnitude;
+        }
 
         [Header("Events")]
         [SerializeField]
--- Assets/Scripts/Runtime/SpeedTracker.cs
+++ Assets/Scripts/Runtime/SpeedTracker.cs
@@ -1,36 +1,58 @@
-using System.Collections.Generic;
 using MyBox;
 using UnityEngine;
 
 namespace ZSBB {
     sealed class SpeedTracker : MonoBehaviour {
-        [SerializeField, ReadOnly]
-        float m_currentSpeed;
-        public float currentSpeed => m_currentSpeed;
+        record Snapshot(Vector3 position, float deltaTime);
 
-        [SerializeField]
-        int bufferSize = 64;
-        Queue<(Vector3 position, float deltaTime)> positionBuffer = new();
+        public Vector3 position => transform.position;
+
+        [SerializeField, Range(0, 10)]
+        float m_speedSmoothing = 1f / 32;
+
+        [SerializeField, Range(0, 10)]
+        float m_isMovingThreshold = 1f / 8;
+
+        [Header("Runtime")]
+        [SerializeField, ReadOnly]
+        Vector3 m_currentVelocity;
+        public Vector3 velocity => m_currentVelocity;
+
+        [SerializeField, ReadOnly]
+        Vector3 m_direction;
+        public Vector3 direction => m_direction;
+
+        [SerializeField, ReadOnly]
+        float m_speed;
+        public float speed => m_speed;
+
+        [SerializeField, ReadOnly]
+        bool m_isMoving;
+        public bool isMoving => m_isMoving;
+
+        Snapshot previousSnapshot;
+        Snapshot currentSnapshot => new(transform.position, Time.deltaTime);
+
+        void Awake() {
+            previousSnapshot = currentSnapshot;
+        }
 
         void FixedUpdate() {
-            // Füge die aktuelle Position in den Puffer hinzu
-            positionBuffer.Enqueue((transform.position, Time.deltaTime));
+            var nextSnapshot = currentSnapshot;
 
-            // Wenn der Puffer voll ist, entferne die älteste Position
-            if (positionBuffer.Count > bufferSize) {
-                positionBuffer.Dequeue();
-            }
+            Vector3.SmoothDamp(
+                previousSnapshot.position,
+                nextSnapshot.position,
+                ref m_currentVelocity,
+                m_speedSmoothing
+            );
+            m_speed = m_currentVelocity.magnitude;
+            m_isMoving = m_speed > m_isMovingThreshold;
+            m_direction = m_isMoving
+                ? Vector3.zero
+                : m_currentVelocity.normalized;
 
-            // Berechne die durchschnittliche Geschwindigkeit basierend auf der Distanz zwischen allen aufeinanderfolgenden Positionen im Puffer
-            float totalDistance = 0f;
-            float totalTime = 0f;
-            var positions = positionBuffer.ToArray();
-            for (int i = 1; i < positions.Length; i++) {
-                float distance = Vector3.Distance(positions[i].position, positions[i - 1].position);
-                totalDistance += distance / i;
-                totalTime += positions[i].deltaTime / i;
-            }
-            m_currentSpeed = totalDistance / totalTime;
+            previousSnapshot = nextSnapshot;
         }
     }
 }
