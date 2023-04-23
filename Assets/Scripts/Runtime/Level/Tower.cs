e01c4f9e5aad6920c64cc2e3862eadd69bf0a63f
Daniel Schulz <faulolio@gmail.com>
2023-04-23 13:00:41 +0200

    Add PlayerTower

---
+++ Assets/Scripts/Runtime/Level/Tower.cs
@@ -0,0 +1,51 @@
+using Slothsoft.UnityExtensions;
+using UnityEngine;
+
+namespace ZSBB.Level {
+    [ExecuteAlways]
+    sealed class Tower : MonoBehaviour {
+        [SerializeField]
+        GameObject segmentPrefab;
+        [SerializeField, Range(0, 100)]
+        int segmentCount = 0;
+        [SerializeField, Range(0, 10)]
+        int segmentHeight = 2;
+        [SerializeField]
+        GameObject topPrefab;
+
+#if UNITY_EDITOR
+        bool isDirty = false;
+        void OnValidate() {
+            isDirty = true;
+        }
+        void Update() {
+            if (!Application.isPlaying && isDirty) {
+                isDirty = false;
+                SpawnTower();
+            }
+        }
+#endif
+        void Awake() {
+            if (Application.isPlaying) {
+                SpawnTower();
+            }
+        }
+        void SpawnTower() {
+            transform.Clear();
+            if (segmentPrefab) {
+                for (int i = 0; i < segmentCount; i++) {
+                    float y = i * segmentHeight;
+                    var instance = Instantiate(segmentPrefab, transform);
+                    instance.hideFlags = HideFlags.DontSave;
+                    instance.transform.localPosition = Vector3.up * y;
+                }
+            }
+            if (topPrefab) {
+                float y = segmentCount * segmentHeight;
+                var instance = Instantiate(topPrefab, transform);
+                instance.hideFlags = HideFlags.DontSave;
+                instance.transform.localPosition = Vector3.up * y;
+            }
+        }
+    }
+}
